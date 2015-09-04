b9cb949eb9ec5ecb419211d253c6f8c2b82cdeb6
lkdjiin <xavier.nayrac@gmail.com>
2015-09-04 20:08:58 +0200

    Add some tests

---
+++ spec/base_rule_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+include Electr
+
+describe BaseRule do
+
+  it "is not implemented" do
+    expect {
+      BaseRule.new(nil, nil).apply!
+    }.to raise_error(NotImplementedError)
+  end
+
+end
