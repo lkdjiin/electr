cf65e404e899e80b18873a33ddd7ab93dd4ab168
lkdjiin <xavier.nayrac@gmail.com>
2015-09-05 20:54:59 +0200

    Refactor AST#display

--- lib/electr/ast/ast.rb
+++ lib/electr/ast/ast.rb
@@ -33,18 +33,28 @@ module Electr
     end
 
     def display(indent = 0)
-      print " " * indent + @name
-      if leaf?
-        print " ::= #@value"
-      else
-        print " (#@value)" if @value
-      end
+      print name_for_display(indent)
+      print value_for_display
       puts
       @children.each {|child| child.display(indent + 2) }
     end
 
     def to_pn
       [* Pn.new(@value, @name)] + @children.map {|child| child.to_pn }.flatten
+    end
+
+    private
+
+    def name_for_display(indent)
+      " " * indent + @name
+    end
+
+    def value_for_display
+      if leaf?
+        " ::= #@value"
+      else
+        " (#@value)" if @value
+      end
     end
 
   end
