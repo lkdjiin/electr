74dd40c68034e692c82e3d748463cb3ed4c53c29
lkdjiin <xavier.nayrac@gmail.com>
2015-11-04 15:32:51 +0100

    Print an ElectrValue instead of a simple number

--- spec/repl/printer_spec.rb
+++ spec/repl/printer_spec.rb
@@ -6,12 +6,44 @@ describe Printer do
 
   describe ".run" do
 
+    it 'accepts ElectrValue objects' do
+      expect {
+
+        Printer.run(ElectrValue.new(10.0))
+
+      }.not_to raise_error
+    end
+
+    it 'accepts only ElectrValue objects' do
+      expect {
+
+        Printer.run(10.0)
+
+      }.to raise_error
+    end
+
     it 'truncates integer like number' do
-      expect { Printer.run(10.0) }.to output("10\n").to_stdout
+      expect {
+
+        Printer.run(ElectrValue.new(10.0))
+
+      }.to output("10\n").to_stdout
     end
 
     it 'rounds number' do
-      expect { Printer.run(1.0 / 3) }.to output("0.3333333333\n").to_stdout
+      expect {
+
+        Printer.run(ElectrValue.new(1.0 / 3))
+
+      }.to output("0.3333333333\n").to_stdout
+    end
+
+    it 'display the error message if any' do
+      expect {
+
+        Printer.run(ElectrValue.error("foo"))
+
+      }.to output("foo\n").to_stdout
     end
 
   end
